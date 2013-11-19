5e7b991d49da118b16fbbd87de73ca39ac31b40c
a-zverev <alexander.zverev@gmail.com>
2013-11-19 14:21:54 +0300

    cross-browser issues

--- script.js
+++ script.js
@@ -16,6 +16,23 @@ function bindReady(handler) {
 }
 
 function loadData(handler) {
+	function getXmlHttp(){
+		var xmlhttp;
+		try {
+			xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try {
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (E) {
+				xmlhttp = false;
+			}
+		}
+		if (!xmlhttp && typeof XMLHttpRequest != 'undefined') {
+			xmlhttp = new XMLHttpRequest();
+		}
+		return xmlhttp;	
+	}
+
 	// Data to load
 	var data = {
 		image: {
@@ -35,20 +52,30 @@ function loadData(handler) {
 		data.image.width = dataCanvas.width = sourceImage.width;
 		data.image.height = dataCanvas.height = sourceImage.height;
 		var dctx = dataCanvas.getContext('2d'); 
-		dctx.drawImage(sourceImage, 0, 0);
-		data.image.pixels = dctx.getImageData(0, 0, dataCanvas.width, dataCanvas.height);
+		console.log(sourceImage.width);
+		try {
+			dctx.drawImage(sourceImage, 0, 0);
+			data.image.pixels = dctx.getImageData(0, 0, dataCanvas.width, dataCanvas.height);
+		} catch (e) {
+			console.log(e);
+		}
 	}
 
 	// Loading palette
-	var xmlHttp = new XMLHttpRequest();
+	var xmlHttp = new getXmlHttp();
+	console.log(undefined);
 	xmlHttp.open('GET', 'legend_example.icxleg', true);
 	xmlHttp.onreadystatechange = function() {
 		if (xmlHttp.readyState == 4) {
 			if (xmlHttp.status == 200) {
 				var responseXML = (new DOMParser()).parseFromString(xmlHttp.responseText, 'text/xml');
-				var paletteNodes = responseXML.children[0].children[0].children;
+				console.log({Pa:responseXML});
+				var paletteNodes = responseXML.firstChild.firstElementChild.childNodes;
 				var palette = [];
+				console.log(paletteNodes);
 				for (var i = 0; i < paletteNodes.length; i++) {
+					if (paletteNodes[i].nodeType == 3)
+						continue;
 					var code = ((paletteNodes[i].getElementsByTagName('Part_Red')[0].textContent)/1);
 					palette[code] = {
 						r: ((paletteNodes[i].getElementsByTagName('Part_Red')[0].textContent)/1),
@@ -74,12 +101,7 @@ function loadData(handler) {
 	isReady();
 }
 
-bindReady(function() {
-	var canvas = document.getElementById('main-canvas');
-	if (typeof canvas.getContext !== "function")
-		throw { messages: "canvas is not supported" };
-	var ctx = canvas.getContext('2d');
-
+function render(canvas, data) {
 	function fixPalette(palette) {
 		var previous;
 		for(var i = 0; i < 256; i++) {
@@ -91,24 +113,35 @@ bindReady(function() {
 		return palette;
 	}
 
-	console.log("loading...");
-	loadData(function(data) {
-		console.log("drawing...");
-		var palette = data.palette;
-		var pixels = data.image.pixels.data;
-		palette = fixPalette(palette);
-		for (var i = 0; i < pixels.length; i+=4) {
-			var brightness = pixels[i];
-			if(typeof palette[brightness] == "undefined")
-				continue;
-			pixels[i] = palette[brightness].r;
-			pixels[i+1] = palette[brightness].g;
-			pixels[i+2] = palette[brightness].b;
-			//pixels[i+1] = 76;
-			//console.log(pixels[i] + "." + pixels[i+1] + "." + pixels[i+2]);
-		}
-		canvas.width = data.image.width;
-		canvas.height = data.image.height;
-		ctx.putImageData(data.image.pixels, 0, 0);
-	});
+	var ctx = canvas.getContext('2d');
+	var palette = data.palette;
+	var pixels = data.image.pixels.data;
+
+	palette = fixPalette(palette);
+	
+	for (var i = 0; i < pixels.length; i += 4) {
+		var brightness = pixels[i];
+		if(typeof palette[brightness] == "undefined")
+			continue;
+		pixels[i] = palette[brightness].r;
+		pixels[i+1] = palette[brightness].g;
+		pixels[i+2] = palette[brightness].b;
+	}
+	canvas.width = data.image.width;
+	canvas.height = data.image.height;
+	ctx.putImageData(data.image.pixels, 0, 0);
+}
+
+bindReady(function() {
+	var canvas = document.getElementById('main-canvas');
+	if (typeof canvas.getContext !== "function") {
+		//throw { message: "canvas is not supported" };
+		console.log("canvas is not supported");
+	} else {
+		console.log("loading...");
+		loadData(function(data) {
+			console.log("drawing...");
+			render(canvas, data);
+		});
+	}
 });
